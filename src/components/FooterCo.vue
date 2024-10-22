a0a281f2c71e4cc8adc714ffc19a19a9263c00e6
Dilan Marcano <d.marcano@DAKA.LOCAL>
2024-10-22 10:03:57 -0400

    check base styles and resposnive desing

--- src/components/FooterCo.vue
+++ src/components/FooterCo.vue
@@ -5,8 +5,15 @@ const mode = useModeStore()
 </script>
 
 <template>
-  <footer class="eme-text-center">
-    <a class="segoe-UI" href="/">&copy; 2024 Dilan Marcano. Todos los derechos reservados.
+  <!-- <footer class="eme-text-center"> -->
+  <footer
+    :class="[
+      'principal-trasition eme-text-center',
+      mode.isLightMode ? 'eme-footer-light' : 'eme-footer-dark',
+    ]"
+  >
+    <a class="segoe-UI" href="/"
+      >&copy; 2024 Dilan Marcano. Todos los derechos reservados.
     </a>
   </footer>
 </template>
